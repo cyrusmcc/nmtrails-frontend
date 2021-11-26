711d4cdba07e049dfca2b227315aa151af55ffee
cyrusmcc <cyrusmcc@hotmail.com>
2021-11-26 15:20:42 -0700

    Add featured trails and region components

---
+++ src/components/FeaturedRegion.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="mapContainer">
+    <img
+      id="mapBackground"
+      src="../assets/imgs/green-gradient.svg"
+      alt="gradient background"
+    />
+    <l-map id="map" :options="{ attributionControl: false }">
+      <l-tile-layer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      ></l-tile-layer>
+      <l-geo-json :geojson="geojson" :options="geojsonOptions"></l-geo-json>
+    </l-map>
+  </div>
+</template>
+
+<script>
+import "leaflet/dist/leaflet.css";
+import { LMap, LTileLayer, LGeoJson } from "@vue-leaflet/vue-leaflet";
+
+export default {
+  name: "FeaturedRegion",
+  components: {
+    LMap,
+    LGeoJson,
+    LTileLayer,
+  },
+  data() {
+    return {
+      geojson: {
+        type: "FeatureCollection",
+        features: [
+          {
+            type: "Feature",
+            properties: {
+              name: "Albuquerque",
+              description: "New Mexico",
+              "marker-color": "#ff8888",
+              "marker-size": "large",
+              "marker-symbol": "mountain",
+            },
+            geometry: {
+              type: "Point",
+              coordinates: [-106.62930965423584, 35.10537479831707],
+            },
+          },
+        ],
+      },
+      geojsonOptions: {
+        style: {
+          color: "#ff8888",
+          weight: 2,
+          opacity: 1,
+        },
+        wrapLatLng: true,
+      },
+    };
+  },
+  //async beforeMount () {
+  //const { circleMarker } = await import("leaflet/dist/leaflet.css");
+
+  //this.geojsonOptions.pointToLayer = (feature, latLng) =>
+  //  circleMarker(latLng, { radius: 8 });
+  //this.mapIsReady = true;
+
+  // }
+};
+</script>
+
+<style scoped lang="scss">
+.mapContainer {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  position: relative;
+  width: 100%;
+  height: 15rem;
+  margin-top: 20px;
+}
+
+#mapBackground {
+  position: absolute;
+  left: 0;
+  right: 0;
+  margin: 0 auto 0 auto;
+  width: 95%;
+  height: 100%;
+  box-shadow: $shadow;
+  border-radius: 4px;
+}
+
+#map {
+  max-width: 90%;
+  max-height: 90%;
+}
+</style>
---
+++ src/components/FeaturedTrails.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="carouselContainer">
+    <div class="shadow"></div>
+    <n-carousel class="carousel" show-arrow>
+      <div id="featuredTrail">
+        <div id="featuredTrailText">
+          <span id="featuredTrailLabel">Featured Trail</span>
+          <span id="featuredTrailName">trail name</span>
+        </div>
+        <img class="carousel-img" src="../assets/imgs/hiking.jpg" />
+      </div>
+    </n-carousel>
+  </div>
+</template>
+
+<script>
+import { NCarousel } from "naive-ui";
+
+export default {
+  name: "FeaturedTrails",
+  components: {
+    NCarousel,
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.carouselContainer {
+  position: relative;
+}
+
+.carousel {
+  line-height: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+}
+
+.carousel-img {
+  width: 100%;
+  height: 240px;
+  object-fit: cover;
+}
+
+.shadow {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 240px;
+  z-index: 2;
+  box-shadow: $shadowInset;
+  pointer-events: none;
+}
+
+#featuredTrailText {
+  display: flex;
+  flex-direction: column;
+  align-text: center;
+  position: absolute;
+  bottom: 0;
+  z-index: 1;
+  line-height: 1;
+  background-color: $primaryLight;
+  box-shadow: $shadow;
+  margin: 0 0 35px 20px;
+  padding: 5px;
+  border-radius: 4px;
+  row-gap: 5px;
+}
+
+#featuredTrailLabel {
+  font-size: 1rem;
+  font-weight: bold;
+  text-align: center;
+}
+
+#featuredTrailName {
+  font-style: italic;
+  text-align: center;
+}
+</style>
