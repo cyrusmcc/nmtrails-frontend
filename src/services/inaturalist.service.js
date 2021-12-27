87d9c0b0172ac86d73f958e1897a3ab0f48c84b1
cyrusmcc <cyrusmcc@hotmail.com>
2021-12-27 11:46:31 -0700

    Add to hike button

--- src/services/inaturalist.service.js
+++ src/services/inaturalist.service.js
@@ -8,28 +8,19 @@ const instance = axios.create({
 });
 
 class INaturalistService {
-  /*
-  getObservations() {
-    return instance
-        .get("observations.json", { params: {
-            "quality_grade" : "research"
-        }})
-        .then((response) => response.data);
-  }
-  */
   getObservations(boundingBox) {
     let coords = boundingBox.coordinates[0];
-    console.log(coords);
 
     let sw = coords[0];
     let ne = coords[2];
     return instance
       .get("observations.json", { params: {
-          "swlat" : sw[1],
+          "swlat" : sw[1],  
           "swlng" : sw[0],
           "nelat" : ne[1],
           "nelng" : ne[0],
-          "quality_grade" : "research"
+          "quality_grade" : "research",
+          "per_page": 5,
       }})
       .then((response) => response.data);
   }
